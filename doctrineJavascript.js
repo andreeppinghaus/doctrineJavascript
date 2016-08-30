a3952b7bc38b3478a66699a83e69d444af5fbdd8
drupal module survey online Zend Framework 2 DEV <andreeppinghaus@gmail.com>
2016-08-30 16:11:38 -0300

    melhorias no código
    Acréscimo de métodos para retorno de consultas retornaAll() e métodos de lista vazia vazio()

--- doctrineJavascript.js
+++ doctrineJavascript.js
@@ -1,5 +1,6 @@
 function DoctrineJavascript (tabela,campos, id){
 	/**
+	 * Esta classe foi preparada para funcionar com sqlite
 	 * tabela = nome da tabela
 	 * campos = estrutura dos campos da tabela
 	 * campos={'codigo': 'integer', 'nome':'text', 'data':'data'};
@@ -29,12 +30,13 @@ function DoctrineJavascript (tabela,campos, id){
 		//console.log(this.obj);
 	  },
 	  
-	  this.getAll = function() {
-		var obj = this.obj;
-		for (var i in obj) {
-			console.log(i,obj[i]);
-		}
-	  },
+//	  this.getAll = function() {
+//		var obj = this.campos;
+//		for (var i in obj) {
+//			console.log(i,obj[i]);
+//		}
+//		
+//	  },
 
 	  this.insert = function() {
 		var obj = this.obj;
@@ -61,6 +63,7 @@ function DoctrineJavascript (tabela,campos, id){
 		sql2 += ' ('+variaveis+') ';
 		sql2 +=' values('+sql.slice(0,(sql.length-1))+')';
 		console.log(sql2);
+		return sql2;
 	  },
 
 	  this.update = function() {
@@ -83,6 +86,8 @@ function DoctrineJavascript (tabela,campos, id){
 		sql = 'update '+this.tabela+' set '+sql.slice(0,(sql.length-1));
 		sql += ' where '+where;
 		console.log(sql);
+		return sql;
+		
 	  }
 
 	  this.remove = function() {
@@ -98,12 +103,13 @@ function DoctrineJavascript (tabela,campos, id){
 		sql = 'delete '+this.tabela;
 		sql += ' where '+where;
 		console.log(sql);
+		return sql;
 	  },
 	  
 	  this.save = function(dados) {
 		this.hydrator(dados);
 		var obj = this.obj;
-		var insere=0;
+		var insere=0, sql;
 		for (var i in obj) {
 			//console.log(i,obj[i]);
 			if (this.id==i && obj[i] == undefined) {
@@ -112,10 +118,109 @@ function DoctrineJavascript (tabela,campos, id){
 		}
 		
 		if(insere==1) {
-			this.insert();
+			sql = this.insert();
 		}else {
-			this.update();
+			sql = this.update();
 		}
-		//console.log(insere);
-	  }
-}
+		return sql;
+	  },
+	  
+	  this.create = function() {
+			var obj = this.campos;
+			var ddl='', sql='';
+			for (var i in obj) {
+			//	console.log(i,obj[i]);
+				if (this.id==i) {
+					ddl+= i+' integer primary key,';
+				}else if ( obj[i] != undefined) {
+		    			ddl+= i+" "+obj[i]+",";
+				}
+			}
+			ddl = ddl.slice(0,  (ddl.length-1));
+			sql = 'CREATE TABLE IF NOT EXISTS '+this.tabela+' ('+ddl+')';
+
+			console.log(sql);
+			return sql;
+		  },
+		  
+		  this.remove = function() {
+				var  sql='';
+				sql = 'DROP TABLE IF EXISTS '+this.tabela;
+				console.log(sql);
+				return sql;
+			  },
+
+		  this.getTabela = function() {
+				return this.tabela;
+		  },
+		  
+		  this.getId = function() {
+				return this.id;
+		  },
+		  
+		  this.getCampos = function() {
+				return this.campos;
+		  },
+		  
+		  this.getAll = function(id) {
+			  var where='';
+			  if (id != null){
+				  where = ' where '+ this.getId()+' = '+id;
+			  }
+			  
+			  var sql = {"sql": 'select * from '+this.tabela+where+' order by 1',
+					   "campos": this.campos};
+			//				var sql = 'select * from '+this.tabela+' order by 1';
+			//				console.log(sql);
+			return sql;
+		  },
+		  
+		  this.retornaAll = function(objeto, results) {
+			  var retorno = [], montaobjeto='';
+			  //console.log('total de linhas'+results.rows.length);
+ 			  for (var i = 0;i<results.rows.length;i++){
+ 				  montaobjeto ="{"; //tem que zerar a variavel
+              	//console.log(objeto.campos);
+ 				  for (var campo in objeto.campos) {
+ 					  montaobjeto += '"'+campo +'":"'+results.rows.item(i)[campo]+'",';	
+ 				  }//fim for
+ 				 montaobjeto = montaobjeto.slice(0,(montaobjeto.length-1));
+              	montaobjeto +="}";
+
+              	//console.log('aqui'+montaobjeto+'<--');
+              	retorno.push(JSON.parse(montaobjeto));
+              //	console.log(results.rows.item(i).codigo+'-'+results.rows.item(i).nome);
+              	
+              }//fim for
+ 			  
+ 			  //se existe where enato retorna somente a posicao zero
+ 			  if ( where.length > 0 ){
+ 				  return retorno[0];
+ 			  }else {
+ 				 return retorno;  
+ 			  }
+ 			  
+		  },
+
+		  this.vazio = function(objeto) {
+			  var retorno = [], montaobjeto='';
+			  //console.log('total de linhas'+results.rows.length);
+ 				  montaobjeto ="{"; //tem que zerar a variavel
+              	//console.log(objeto.campos);
+ 				  for (var campo in objeto.campos) {
+ 					  montaobjeto += '"'+campo +'":"lista vazia",';	
+ 				  }//fim for
+ 				 montaobjeto = montaobjeto.slice(0,(montaobjeto.length-1));
+              	montaobjeto +="}";
+
+              	//console.log('aqui'+montaobjeto+'<--');
+              	retorno.push(JSON.parse(montaobjeto));
+              //	console.log(results.rows.item(i).codigo+'-'+results.rows.item(i).nome);
+              	
+ 			  return retorno;
+		  }
+
+
+}//fim doctrine
+
+
